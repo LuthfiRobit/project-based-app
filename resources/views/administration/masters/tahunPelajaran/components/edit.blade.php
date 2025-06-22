b99161759423b96643ff492eb491d431ad08df53
LuthfiRobit <luthfirobit@gmail.com>
2025-06-22 11:01:49 +0700

    make tahun pelajaran scripts and views

---
+++ resources/views/administration/masters/tahunPelajaran/components/edit.blade.php
@@ -0,0 +1,37 @@
+<!-- Modal Edit Start -->
+<div class="modal fade" id="modalEdit" tabindex="-1" role="dialog" aria-hidden="true" data-bs-backdrop="static">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalEditLabel">Edit Data</h5>
+                <button type="button" class="btn-close" aria-label="Close" data-bs-dismiss="modal"></button>
+            </div>
+            <div class="modal-body">
+                <div id="modalError" class="alert alert-danger d-none" role="alert"></div>
+                <!-- Alert for general errors -->
+                <form id="editForm" method="post" class="form-sm" data-id="">
+                    <div class="mb-3">
+                        <label for="nama_tahun_pelajaran" class="form-label">Tahun Pelajaran</label>
+                        <input type="text" class="form-control form-control-sm" id="nama_tahun_pelajaran"
+                            name="nama_tahun_pelajaran" placeholder="Masukkan jabatan" aria-label="Nama Tahun Ajaran"
+                            maxlength="100" autocomplete="off" />
+                    </div>
+                    {{-- <div class="mb-3">
+                        <label for="status" class="form-label">Status</label>
+                        <select id="status" name="status" class="selectpicker form-control wide form-select-md"
+                            data-live-search="false" required aria-describedby="instansi-feedback"
+                            aria-label="Pilih Status">
+                            <option value="active">Aktif</option>
+                            <option value="inactive">Tidak aktif</option>
+                        </select>
+                    </div> --}}
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" form="editForm" class="btn btn-primary">Simpan Perubahan</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal Edit End -->
