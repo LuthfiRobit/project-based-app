dbe9a75968d83dd11000b88aabac8ab6a4d90726
LuthfiRobit <luthfirobit@gmail.com>
2025-06-22 11:04:09 +0700

    modify web routes needed

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,16 @@
 
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Master\GuruController;
+use App\Http\Controllers\Master\IuranController;
 use App\Http\Controllers\Master\JabatanGuruController;
+use App\Http\Controllers\Master\JurusanController;
+use App\Http\Controllers\Master\KeringananController;
+use App\Http\Controllers\Master\RuangKelasController;
+use App\Http\Controllers\Master\SekolahController;
+use App\Http\Controllers\Master\SemesterController;
+use App\Http\Controllers\Master\SiswaController;
+use App\Http\Controllers\Master\TahunPelajaranController;
+use App\Http\Controllers\Master\TingkatController;
 use App\Http\Controllers\RBAC\PermissionController;
 use App\Http\Controllers\RBAC\RoleController;
 use App\Http\Controllers\RBAC\UserController;
@@ -14,9 +23,16 @@ use App\Http\Middleware\CheckPermission;
 
 // Public routes
 Route::get('/', fn() => view('welcome'));
-Route::get('login', [AuthController::class, 'loginView'])->name('login.view');
-Route::post('login', [AuthController::class, 'login'])->name('login');
-Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+// Auth routes
+Route::middleware('guest')->group(function () {
+    Route::get('login', [AuthController::class, 'loginView'])->name('login.view');
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+});
 
 // Protected routes (auth + permission check by route name)
 Route::middleware(['auth', 'checkPermission'])->group(function () {
@@ -24,6 +40,28 @@ Route::middleware(['auth', 'checkPermission'])->group(function () {
 
     // Master
     Route::prefix('master')->name('master.')->group(function () {
+        Route::prefix('sekolah')->name('sekolah.')->group(function () {
+            Route::get('/', [SekolahController::class, 'index'])->name('index');
+        });
+
+        Route::prefix('tingkat')->name('tingkat.')->group(function () {
+            Route::get('/', [TingkatController::class, 'index'])->name('index');
+            Route::get('/list', [TingkatController::class, 'list'])->name('list');
+            Route::post('/store', [TingkatController::class, 'store'])->name('store');
+            Route::get('/show/{id}', [TingkatController::class, 'show'])->name('show');
+            Route::put('/update/{id}', [TingkatController::class, 'update'])->name('update');
+            Route::post('/update-status-multiple', [TingkatController::class, 'updateStatusMultiple'])->name('update-status-multiple');
+        });
+
+        Route::prefix('jurusan')->name('jurusan.')->group(function () {
+            Route::get('/', [JurusanController::class, 'index'])->name('index');
+            Route::get('/list', [JurusanController::class, 'list'])->name('list');
+        });
+
+        Route::prefix('ruang-kelas')->name('ruang-kelas.')->group(function () {
+            Route::get('/', [RuangKelasController::class, 'index'])->name('index');
+            Route::get('/list', [RuangKelasController::class, 'list'])->name('list');
+        });
 
         Route::prefix('jabatan-guru')->name('jabatan-guru.')->group(function () {
             Route::get('/', [JabatanGuruController::class, 'index'])->name('index');
@@ -43,6 +81,48 @@ Route::middleware(['auth', 'checkPermission'])->group(function () {
             Route::get('/show/{id}', [GuruController::class, 'show'])->name('show');
             Route::get('/edit/{id}', [GuruController::class, 'edit'])->name('edit');
             Route::put('/update/{id}', [GuruController::class, 'update'])->name('update');
+        });
+
+        Route::prefix('tahun-pelajaran')->name('tahun-pelajaran.')->group(function () {
+            Route::get('/', [TahunPelajaranController::class, 'index'])->name('index');
+            Route::get('/list', [TahunPelajaranController::class, 'list'])->name('list');
+            Route::post('/store', [TahunPelajaranController::class, 'store'])->name('store');
+            Route::get('/show/{id}', [TahunPelajaranController::class, 'show'])->name('show');
+            Route::put('/update/{id}', [TahunPelajaranController::class, 'update'])->name('update');
+            Route::post('/update-status-single', [TahunPelajaranController::class, 'updateStatusSingle'])->name('update-status-single');
+        });
+
+        Route::prefix('semester')->name('semester.')->group(function () {
+            Route::get('/', [SemesterController::class, 'index'])->name('index');
+            Route::get('/list', [SemesterController::class, 'list'])->name('list');
+            Route::post('/store', [SemesterController::class, 'store'])->name('store');
+            Route::get('/show/{id}', [SemesterController::class, 'show'])->name('show');
+            Route::put('/update/{id}', [SemesterController::class, 'update'])->name('update');
+            Route::post('/update-status-single', [SemesterController::class, 'updateStatusSingle'])->name('update-status-single');
+        });
+
+        Route::prefix('iuran')->name('iuran.')->group(function () {
+            Route::get('/', [IuranController::class, 'index'])->name('index');
+            Route::get('/list', [IuranController::class, 'list'])->name('list');
+            Route::post('/store', [IuranController::class, 'store'])->name('store');
+            Route::get('/show/{id}', [IuranController::class, 'show'])->name('show');
+            Route::put('/update/{id}', [IuranController::class, 'update'])->name('update');
+            Route::post('/update-status-multiple', [IuranController::class, 'updateStatusMultiple'])->name('update-status-multiple');
+            // Route::post('/import-excel', [IuranController::class, 'importExcel'])->name('import-excel');
+        });
+
+        Route::prefix('keringanan')->name('keringanan.')->group(function () {
+            Route::get('/', [KeringananController::class, 'index'])->name('index');
+            Route::get('/list', [KeringananController::class, 'list'])->name('list');
+            Route::post('/store', [KeringananController::class, 'store'])->name('store');
+            Route::get('/show/{id}', [KeringananController::class, 'show'])->name('show');
+            Route::put('/update/{id}', [KeringananController::class, 'update'])->name('update');
+            Route::post('/update-status-multiple', [KeringananController::class, 'updateStatusMultiple'])->name('update-status-multiple');
+            // Route::post('/import-excel', [IuranController::class, 'importExcel'])->name('import-excel');
+        });
+
+        Route::prefix('siswa')->name('siswa.')->group(function () {
+            Route::get('/', [SiswaController::class, 'index'])->name('index');
         });
     });
 
